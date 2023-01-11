73de84de24b9e2b216153faf6f80f40d328dec69
JoacoCinnamon <joacopontello2002@gmail.com>
2023-01-11 19:45:08 -0300

    Force role user for Register

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -32,7 +32,7 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:' . User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
@@ -41,6 +41,10 @@ class RegisteredUserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
+
+        // Cuando un usuario se registra debe tener el rol "user" predeterminado
+        $user->syncRoles('user');
+
 
         event(new Registered($user));
 
