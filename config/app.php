f25bd1731d759e09303cbc48cc43a81ed321c9d4
JoacoCinnamon <joacopontello2002@gmail.com>
2023-01-11 03:39:44 -0300

    Install spatie/laravel-permission initial setup

--- config/app.php
+++ config/app.php
@@ -195,6 +195,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /**
+         * Spatie Service Providers...
+         */
+        Spatie\Permission\PermissionServiceProvider::class,
     ],
 
     /*
