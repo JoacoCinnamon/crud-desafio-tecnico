a325a8fd38a0f65b98d1aecc3acd4d06dafd3daa
JoacoCinnamon <joacopontello2002@gmail.com>
2023-01-14 20:58:10 -0300

    Revert "test delete composer.lock to deploy"
    This reverts commit 874f1073d232ba886ca134b58616642af2a5aa8d.

--- resources/views/admin/users/show.blade.php
+++ resources/views/admin/users/show.blade.php
@@ -2,7 +2,8 @@
     <div class="flex flex-row min-h-screen justify-center items-center p-12">
         <div class="rounded-lg shadow-lg bg-white max-w-sm text-center">
             <div class="p-3 px-6 border-b border-gray-300">
-                {{ Str::ucfirst($user->name) }}
+                {{ Str::ucfirst($user->name) }}<br>
+                {{ $user->email }}
             </div>
             <div class="p-6">
                 <h5 class="text-gray-900 text-xl font-medium mb-2">Roles</h5>
